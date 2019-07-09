810839e9da632e4d5a53a430c43154d0e2fd398c
Anliner Leona <anliner@webriq.services>
2019-07-09 14:23:17 +0800

    update navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -42,7 +42,7 @@ export default class Navbar extends Component {
 					<span className="navbar-toggler-icon" />
 				</button>
 				<div className={this.state.css}>
-					<ul className="navbar-nav max-auto">
+					<ul className="navbar-nav mx-auto">
 						{this.state.links.map(link => {
 							return (
 								<li key={link.id} className="nav-item">
